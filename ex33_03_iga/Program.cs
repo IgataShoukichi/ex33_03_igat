0637fa6d202c92a71bbb306958824baf19e83fa4
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2022-12-22 16:25:56 +0900

    面積追加

--- ex33_03_iga/Program.cs
+++ ex33_03_iga/Program.cs
@@ -10,7 +10,7 @@ namespace EX33
             RectAngle rectAngle2 = new RectAngle(2, 4);
             RectAngle rectAngle3 = rectAngle1 + rectAngle2;
 
-            Console.WriteLine($"2つの長方形が入る長方形は({rectAngle3.width},{rectAngle3.height})");
+            Console.WriteLine($"2つの長方形が入る長方形は({rectAngle3.width},{rectAngle3.height}) \n面積は({rectAngle3.height * rectAngle3.width})");
         }
     }
 }
