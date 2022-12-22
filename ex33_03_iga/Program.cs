123f0ff09b9a9d5fc18a7f05b272c5df9f1b04a0
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2022-12-22 15:14:13 +0900

    プロジェクト ファイルを追加します。

---
+++ ex33_03_iga/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EX33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RectAngle rectAngle1 = new RectAngle(3.5f, 5);
+            RectAngle rectAngle2 = new RectAngle(2, 4);
+            RectAngle rectAngle3 = rectAngle1 + rectAngle2;
+
+            Console.WriteLine($"2つの長方形が入る長方形は({rectAngle3.width},{rectAngle3.height})");
+        }
+    }
+}
---
+++ ex33_03_iga/RectAngle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX33
+{
+    class RectAngle : ISurface, ICircumference, IBounds
+    {
+        readonly public float width;
+        readonly public float height;
+        public RectAngle(float width = 0, float height = 0)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public float GetSurface()
+        {
+            return width * height;
+        }
+        public float GetCircumference()
+        {
+            return (width + height) * 2;
+        }
+        public void GetBounds(out float width, out float height)
+        {
+            width = this.width;
+            height = this.height;
+        }
+        public static bool operator ==(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            return rectAngle1.width == rectAngle2.width
+                && rectAngle1.height == rectAngle2.height
+                || rectAngle1.width == rectAngle2.height
+                && rectAngle1.height == rectAngle2.width;
+        }
+
+        public static bool operator !=(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            return !(rectAngle1 == rectAngle2);
+        }
+
+        public static RectAngle operator +(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            int num = 0;
+            RectAngle[] pattern = new RectAngle[4];
+
+            if (rectAngle1.height > rectAngle2.height)
+            {
+                pattern[0] = new RectAngle(rectAngle1.width + rectAngle2.width, rectAngle1.height);
+            }
+            else
+            {
+                pattern[0] = new RectAngle(rectAngle1.width + rectAngle2.width, rectAngle2.height);
+            }
+
+            if (rectAngle1.height > rectAngle2.width)
+            {
+                pattern[1] = new RectAngle(rectAngle1.width + rectAngle2.height, rectAngle1.height);
+            }
+            else
+            {
+                pattern[1] = new RectAngle(rectAngle1.width + rectAngle2.height, rectAngle2.width);
+            }
+
+            if (rectAngle1.width > rectAngle2.height)
+            {
+                pattern[2] = new RectAngle(rectAngle1.height + rectAngle2.width, rectAngle1.width);
+            }
+            else
+            {
+                pattern[2] = new RectAngle(rectAngle1.height + rectAngle2.width, rectAngle2.height);
+            }
+
+            if (rectAngle1.width > rectAngle2.width)
+            {
+                pattern[3] = new RectAngle(rectAngle1.height + rectAngle2.height, rectAngle1.width);
+            }
+            else
+            {
+                pattern[3] = new RectAngle(rectAngle1.height + rectAngle2.height, rectAngle2.width);
+            }
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (pattern[i].width * pattern[i].height < pattern[num].width * pattern[num].height)
+                {
+                    num = i;
+                }
+                else
+                {
+                    num = 0;
+                }
+            }
+            return pattern[num];
+        }
+    }
+}
